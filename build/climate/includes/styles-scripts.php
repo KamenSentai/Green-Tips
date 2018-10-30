9bb76644252aaacaf5ffaca285cbaaa203455de5
AlainKRSS <cvt.alain@gmail.com>
2018-10-30 17:34:37 +0100

    style(): remove spaces

--- build/climate/includes/styles-scripts.php
+++ build/climate/includes/styles-scripts.php
@@ -1,11 +1,11 @@
 <?php
 
 function add_styles_scripts() {
-	wp_register_style('style', CSS_URL.'/app.css');
+	wp_register_style('style', CSS_URL . '/app.css');
 	wp_enqueue_style('style');
 
 	wp_register_script('main', JS_URL . '/app.js', array(),'1.0', true);
 	wp_enqueue_script('main');
 }
 
-add_action( 'wp_enqueue_scripts', 'add_styles_scripts' );
+add_action('wp_enqueue_scripts', 'add_styles_scripts');
