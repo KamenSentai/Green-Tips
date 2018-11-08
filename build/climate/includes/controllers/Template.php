073b8c8242556a46f7dd3fe73a807a52c470de09
AlainKRSS <cvt.alain@gmail.com>
2018-11-08 12:46:22 +0100

    fix(): remove MVC

--- build/climate/includes/controllers/Template.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Climate\Controllers;
-
-class Template {
-	public function __construct($file) {
-		echo '<p>' . $file . '</p>';
-	}
-}
--- build/climate/index.php
+++ build/climate/index.php
@@ -1,15 +1,3 @@
-<?php
-
-namespace Climate;
-
-\Climate\Autoloader::register();
-
-use \Climate\Controllers as CC;
-
-?>
-
 <?php get_header(); ?>
-
-<?php $index = new CC\Template('autoload'); ?>
 
 <?php get_footer(); ?>
