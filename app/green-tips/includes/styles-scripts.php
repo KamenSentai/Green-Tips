b621c7aae08b349ff0013f90475e5d4e0fbe7622
AlainKRSS <cvt.alain@gmail.com>
2018-11-13 12:47:38 +0100

    style(): fix spaces and name of script

--- app/green-tips/includes/styles-scripts.php
+++ app/green-tips/includes/styles-scripts.php
@@ -4,8 +4,8 @@ function add_styles_scripts() {
 	wp_register_style('style', CSS_URL . '/app.css');
 	wp_enqueue_style('style');
 
-	wp_register_script('main', JS_URL . '/app.js', array(), '1.0', true);
-	wp_enqueue_script('main');
+	wp_register_script('script', JS_URL . '/app.js', array(), '1.0', true);
+	wp_enqueue_script('script');
 }
 
 add_action('wp_enqueue_scripts', 'add_styles_scripts');
