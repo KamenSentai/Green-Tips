ececb2bce37e8563354971107b8e0a780d2a40b1
LorisMarino <loris.marino@hetic.net>
2018-11-19 14:49:06 +0100

    fix(tips): height of filters

--- app/green-tips/page-templates/page-tips.php
+++ app/green-tips/page-templates/page-tips.php
@@ -99,6 +99,70 @@
           </p>
           <div class="card-tips__image"></div>
         </div>
+        <div class="card-tips">
+          <div class="card-tips-top">
+            <h2 class="card-tips-top__title">
+              Réduire l'opacité de ses document avant impression
+            </h2>
+            <div class="card-tips-top-more">
+              <span></span>
+              <span></span>
+              <span></span>
+            </div>
+          </div>
+          <p class="card-tips__resume">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet magnam quidem labore magni, reprehenderit dolor in doloremque odit expedita aliquam. Voluptate quis beatae maxime numquam tempore blanditiis hic, perspiciatis obcaecati?
+          </p>
+          <div class="card-tips__image"></div>
+        </div>
+        <div class="card-tips">
+          <div class="card-tips-top">
+            <h2 class="card-tips-top__title">
+              Réduire l'opacité de ses document avant impression
+            </h2>
+            <div class="card-tips-top-more">
+              <span></span>
+              <span></span>
+              <span></span>
+            </div>
+          </div>
+          <p class="card-tips__resume">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet magnam quidem labore magni, reprehenderit dolor in doloremque odit expedita aliquam. Voluptate quis beatae maxime numquam tempore blanditiis hic, perspiciatis obcaecati?
+          </p>
+          <div class="card-tips__image"></div>
+        </div>
+        <div class="card-tips">
+          <div class="card-tips-top">
+            <h2 class="card-tips-top__title">
+              Réduire l'opacité de ses document avant impression
+            </h2>
+            <div class="card-tips-top-more">
+              <span></span>
+              <span></span>
+              <span></span>
+            </div>
+          </div>
+          <p class="card-tips__resume">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet magnam quidem labore magni, reprehenderit dolor in doloremque odit expedita aliquam. Voluptate quis beatae maxime numquam tempore blanditiis hic, perspiciatis obcaecati?
+          </p>
+          <div class="card-tips__image"></div>
+        </div>
+        <div class="card-tips">
+          <div class="card-tips-top">
+            <h2 class="card-tips-top__title">
+              Réduire l'opacité de ses document avant impression
+            </h2>
+            <div class="card-tips-top-more">
+              <span></span>
+              <span></span>
+              <span></span>
+            </div>
+          </div>
+          <p class="card-tips__resume">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet magnam quidem labore magni, reprehenderit dolor in doloremque odit expedita aliquam. Voluptate quis beatae maxime numquam tempore blanditiis hic, perspiciatis obcaecati?
+          </p>
+          <div class="card-tips__image"></div>
+        </div>
       </div>
     </div>
   </div>
