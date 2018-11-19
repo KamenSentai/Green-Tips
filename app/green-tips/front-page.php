ea7e2050d8d9f06c330780ab64be968162011408
LorisMarino <loris.marino@hetic.net>
2018-11-19 14:43:08 +0100

    feat(home): Add popup

--- app/green-tips/front-page.php
+++ app/green-tips/front-page.php
@@ -6,6 +6,33 @@ get_header();
 
 <main id="content">
 
+<div class="popup-tips">
+    <div class="popup-tips-content">
+      <div class="card-tips">
+        <div class="card-tips-top">
+          <h2 class="card-tips-top__title">
+            Réduire l'opacité de ses document avant impression
+          </h2>
+          <div class="card-tips-top-more">
+            <span></span>
+              <span></span>
+            <span></span>
+          </div>
+        </div>
+        <p class="card-tips__resume">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet magnam quidem labore magni, reprehenderit dolor in doloremque odit expedita aliquam. Voluptate quis beatae maxime numquam tempore blanditiis hic, perspiciatis obcaecati?
+          <br /><br />Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet magnam quidem labore magni, reprehenderit dolor in doloremque odit expedita aliquam. Voluptate quis beatae maxime numquam tempore blanditiis hic, perspiciatis obcaecati?
+          <br /><br />Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet magnam quidem labore magni, reprehenderit dolor in doloremque odit expedita aliquam. Voluptate quis beatae maxime numquam tempore blanditiis hic, perspiciatis obcaecati?
+          <br /><br />Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet magnam quidem labore magni, reprehenderit dolor in doloremque odit expedita aliquam. Voluptate quis beatae maxime numquam tempore blanditiis hic, perspiciatis obcaecati?
+          <br /><br />Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet magnam quidem labore magni, reprehenderit dolor in doloremque odit expedita aliquam. Voluptate quis beatae maxime numquam tempore blanditiis hic, perspiciatis obcaecati?
+          <br /><br />Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet magnam quidem labore magni, reprehenderit dolor in doloremque odit expedita aliquam. Voluptate quis beatae maxime numquam tempore blanditiis hic, perspiciatis obcaecati?
+          <br /><br />Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet magnam quidem labore magni, reprehenderit dolor in doloremque odit expedita aliquam. Voluptate quis beatae maxime numquam tempore blanditiis hic, perspiciatis obcaecati?
+        </p>
+        <div class="card-tips__image"></div>
+      </div>
+    </div>
+  </div>
+
 <?php
 
 include LAYOUTS_PATH . '/jumbotron.php';
