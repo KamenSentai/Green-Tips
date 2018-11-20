303697d87f3bf0225a4de1fd791acee3ebef5508
FlorianDurand <contact@floriandurand.com>
2018-11-20 12:19:44 +0100

    feat(single enquete): pokemon lets go

--- app/green-tips/single-enquete.php
+++ app/green-tips/single-enquete.php
@@ -1,24 +1,23 @@
 <?php get_header();?>
 <main id="content" class="enquete">
-
+<?php if ( have_posts() ) :?>
+<?php while ( have_posts() ) : the_post(); ?>
 <div class="header">
 <img src="<?= IMAGES_URL; ?>/test.jpg" class="header__bg">
   <div class="container">
     <div class="header-top">
       <div class="header-top__badge">
-        Enquêtes
+      <?php the_terms( $post->ID, 'tags', '', ' / ' );?>
       </div>
       <div class="header-top__date">
-        10/10/2018
+      <?php echo get_the_date();?>
       </div>
     </div>
     <h1 class="header__title">
-      Présentation orale : Do/Don’t
-      pour améliorer tes soutenances
-      en DUT MMI
+    <?php the_title();?>
     </h1>
     <div class="header__infos">
-      Par Florian | 1000 vues | Aucun commentaire
+      Par <?php echo get_the_author();?> | 1000 vues 
     </div>
   </div>
 </div>
@@ -27,25 +26,11 @@
   <div class="row justify-content-center">
     <div class="col-11 col-md-7">
       <div class="resume">
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Explicabo a, modi dignissimos ea, omnis, placeat ducimus suscipit voluptas fugit amet incidunt. Quisquam, maxime! Velit aliquam similique ex obcaecati temporibus dignissimos?
-        <br /><br />Lorem ipsum dolor sit amet consectetur, adipisicing elit. Explicabo a, modi dignissimos ea, omnis, placeat ducimus suscipit voluptas fugit amet incidunt. Quisquam, maxime! Velit aliquam similique ex obcaecati temporibus dignissimos?
-        <br /><br />Lorem ipsum dolor sit amet consectetur, adipisicing elit. Explicabo a, modi dignissimos ea, omnis, placeat ducimus suscipit voluptas fugit amet incidunt. Quisquam, maxime! Velit aliquam similique ex obcaecati temporibus dignissimos?
-        <br /><br />Lorem ipsum dolor sit amet consectetur, adipisicing elit. Explicabo a, modi dignissimos ea, omnis, placeat ducimus suscipit voluptas fugit amet incidunt. Quisquam, maxime! Velit aliquam similique ex obcaecati temporibus dignissimos?
-        <br /><br />Lorem ipsum dolor sit amet consectetur, adipisicing elit. Explicabo a, modi dignissimos ea, omnis, placeat ducimus suscipit voluptas fugit amet incidunt. Quisquam, maxime! Velit aliquam similique ex obcaecati temporibus dignissimos?
-        <br /><br />Lorem ipsum dolor sit amet consectetur, adipisicing elit. Explicabo a, modi dignissimos ea, omnis, placeat ducimus suscipit voluptas fugit amet incidunt. Quisquam, maxime! Velit aliquam similique ex obcaecati temporibus dignissimos?
+      <?php the_content();?>
       </div>
     </div>
   </div>
 </div>
-
-
-<?php //if ( have_posts() ) :?>
-<?php //while ( have_posts() ) : the_post(); ?>
-<?php //the_terms( $post->ID, 'tags', '', ' / ' );?>
-<?php //echo get_the_date();?>
-<?php //the_title();?>
-<?php //echo get_the_author();?>
-<?php //the_content();?>
-<?php //endwhile; endif; ?>
+<?php endwhile; endif; ?>
 </main>
 <?php get_footer();?>
