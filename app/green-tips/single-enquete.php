370d2a869b8c2d7186c6085625fd79e54c527eaa
LorisMarino <loris.marino@hetic.net>
2018-11-21 12:45:57 +0100

    fix(enquete): Change size

--- app/green-tips/single-enquete.php
+++ app/green-tips/single-enquete.php
@@ -5,26 +5,30 @@
 <div class="header">
 <img src="<?= IMAGES_URL; ?>/test.jpg" class="header__bg">
   <div class="container">
-    <div class="header-top">
-      <div class="header-top__badge">
-      <?php the_terms( $post->ID, 'tags', '', ' / ' );?>
+    <div class="row justify-content-center">
+      <div class="col-12 col-md-8">
+        <div class="header-top">
+          <div class="header-top__badge">
+          <?php the_terms( $post->ID, 'tags', '', ' / ' );?>
+          </div>
+          <div class="header-top__date">
+          <?php echo get_the_date();?>
+          </div>
+        </div>
+        <h1 class="header__title">
+        <?php the_title();?>
+        </h1>
+        <div class="header__infos">
+          Par <?php echo get_the_author();?> | 1000 vues
+        </div>
       </div>
-      <div class="header-top__date">
-      <?php echo get_the_date();?>
-      </div>
-    </div>
-    <h1 class="header__title">
-    <?php the_title();?>
-    </h1>
-    <div class="header__infos">
-      Par <?php echo get_the_author();?> | 1000 vues 
     </div>
   </div>
 </div>
 
 <div class="container">
   <div class="row justify-content-center">
-    <div class="col-11 col-md-7">
+    <div class="col-11 col-md-8">
       <div class="resume">
       <?php the_content();?>
       </div>
