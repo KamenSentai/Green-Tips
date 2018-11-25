1b2f3155af358a067c847cc272fef54f63bfe5f6
LorisMarino <loris.marino@hetic.net>
2018-11-25 18:45:26 +0100

    fix(enquete): apply styles in cards

--- app/green-tips/single-enquete.php
+++ app/green-tips/single-enquete.php
@@ -49,11 +49,11 @@
             {
                  $the_query -> the_post();
                  ?>
-                 <a href="<?php the_permalink();?>" alt=" <?php the_title();?>">
+                 <!--<a href="<?php the_permalink();?>" alt=" <?php the_title();?>">-->
                  <div class="col-12 col-md-4">
                  <div class="card">
                 <img src="<?php the_field('enquete_background_image');?>" class="card__image">
-
+<div class="card-infos">
                   <h2 class="card__title"><?php the_title();?></h2>
                   <div class="card__resume">
                     <?php the_excerpt();?></div>
@@ -61,7 +61,8 @@
                     <a href="<?php the_permalink();?>" class="button-primary">Lire la suite</a>
                   </div>
                 </div>
-              </div> </a>
+            </div>
+              </div> <!--</a>-->
           <?php }}else{
             echo "no result";}
          ?>
