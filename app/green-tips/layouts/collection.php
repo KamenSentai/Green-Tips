a1c30cc34c457deb9cc81f371d8458f32047748e
FlorianDurand <contact@floriandurand.com>
2018-11-25 13:33:18 +0100

    feat(encore des boucles): boucles !

--- app/green-tips/layouts/collection.php
+++ app/green-tips/layouts/collection.php
@@ -1,7 +1,10 @@
 <section id="collection">
+<h3 class="others__title">
+    Voir les dernières enquêtes 
+  </h3>
   <div class="container">
     <div class="row">
-      <div class="col-12 col-md-4">
+      <!-- <div class="col-12 col-md-4">
         <div class="card">
           <img src="<?= IMAGES_URL; ?>/test.jpg" class="card__image">
           <h2 class="card__title">Pékin, ville vapeur sautée</h2>
@@ -11,40 +14,33 @@
             <a href="#" class="button-primary">Lire la suite</a>
           </div>
         </div>
-      </div>
-      <div class="col-12 col-md-4">
-        <div class="card">
-          <div class="card__image"></div>
-          <h2 class="card__title">Pékin, ville vapeur sautée</h2>
-          <p class="card__resume">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia recusandae minus aliquid ratione, saepe distinctio molestias temporibus magni laboriosam enim optio excepturi accusantium minima eligendi qui nam? Libero, sunt possimus.</p>
-          <div class="card__more">
-            <a href="#" class="button-primary">Lire la suite</a>
-          </div>
-        </div>
-      </div>
-      <div class="col-12 col-md-4">
-        <div class="card">
-          <div class="card__image"></div>
-          <h2 class="card__title">Pékin, ville vapeur sautée</h2>
-          <p class="card__resume">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia recusandae minus aliquid ratione, saepe distinctio molestias temporibus magni laboriosam enim optio excepturi accusantium minima eligendi qui nam? Libero, sunt possimus.</p>
-          <div class="card__more">
-            <a href="#" class="button-primary">Lire la suite</a>
-          </div>
-        </div>
-      </div>
-      <div class="col-12 col-md-4">
-        <div class="card">
-          <div class="card__image"></div>
-          <h2 class="card__title">Pékin, ville vapeur sautée</h2>
-          <p class="card__resume">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia recusandae minus aliquid ratione, saepe distinctio molestias temporibus magni laboriosam enim optio excepturi accusantium minima eligendi qui nam? Libero, sunt possimus.</p>
-          <div class="card__more">
-            <a href="#" class="button-primary">Lire la suite</a>
-          </div>
-        </div>
-      </div>
+      </div> -->
+      <?php $enquetes = array(
+            'post_type' => 'enquete'
+         );
+         $the_query = new WP_Query( $enquetes );
+         if($the_query -> have_posts())
+         {
+             while($the_query -> have_posts())
+            {
+                 $the_query -> the_post();
+                 ?>
+                 <a href="<?php the_permalink();?>" alt=" <?php the_title();?>">
+                 <div class="col-12 col-md-4">
+                 <div class="card">
+                <img src="<?php the_field('enquete_background_image');?>" class="card__image">
+
+                  <h2 class="card__title"><?php the_title();?></h2>
+                  <div class="card__resume">
+                    <?php the_excerpt();?></div>
+                  <div class="card__more">
+                    <a href="<?php the_permalink();?>" class="button-primary">Lire la suite</a>
+                  </div>
+                </div>
+              </div> </a>
+          <?php }}else{
+            echo "no result";}
+         ?>
     </div>
   </div>
 </section>
