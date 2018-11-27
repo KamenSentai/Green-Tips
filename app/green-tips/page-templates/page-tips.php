f8432a8084d7d6ae15077b4f1b7ecabcc9fe9e67
AlainKRSS <cvt.alain@gmail.com>
2018-11-27 12:16:11 +0100

    fix(card): fix links

--- app/green-tips/page-templates/page-tips.php
+++ app/green-tips/page-templates/page-tips.php
@@ -3,17 +3,13 @@
 ?>
 <?php get_header();?>
 <main id="content" class="tips">
-
   <div class="container">
+
     <div class="row justify-content-between">
       <div class="col-12 col-md-3">
         <aside id="filters">
-          <h2 class="filters__title">
-            Filtres
-          </h2>
-          <div class="filters-type__title">
-            Popularité
-          </div>
+          <h2 class="filters__title">Filtres</h2>
+          <div class="filters-type__title">Popularité</div>
           <div class="filters-popular">
             <div class="filters-popular-item">
               <input type="radio" id="popular-views" name="popular">
@@ -24,9 +20,7 @@
               <label for="popular-liked">Aimées</label>
             </div>
           </div>
-          <div class="filters-type__title">
-            Catégories
-          </div>
+          <div class="filters-type__title">Catégories</div>
           <div class="filters-cat">
             <div class="filters-cat-item">
               <input type="checkbox" id="cat-energy" name="cat">
@@ -41,40 +35,49 @@
       </div>
 
       <div class="col-12 col-md-8">
-        <?php $tips = array(
-         'post_type' => 'tips'
-         );
-         $the_query = new WP_Query( $tips );
-         if($the_query -> have_posts())
-         {
-             while($the_query -> have_posts())
-             {
-                 $the_query -> the_post();
-                 ?>
-                  <div class="card-tips">
-                    <div class="card-tips-top">
-                      <h2 class="card-tips-top__title">
-                      <?php the_title();?>
-                      </h2>
-                      <div class="card-tips-top-more">
-                        <span></span>
-                        <span></span>
-                        <span></span>
-                      </div>
-                    </div>
-                    <p class="card-tips__resume">
-                    <?= substr(get_field('tips_main_content'), 0, 250) . '...'; ?>
-                    </p>
-                     </div>
-                  <?php
+        <?php
+        $tips = array(
+          'post_type' => 'tips'
+        );
+        $the_query = new WP_Query( $tips );
+        ?>
+        <?php if($the_query -> have_posts()): ?>
+          <?php while($the_query -> have_posts()): ?>
+            <?php $the_query -> the_post(); ?>
+            <div class="card-tips">
+              <div class="card-tips-top">
+                <h2 class="card-tips-top__title">
+                  <?php the_title();?>
+                </h2>
+              </div>
+              <p class="card-tips__resume">
+                <?php
+                $field = get_field('tips_main_content');
+                $field = preg_replace(
+                  '#((https?|ftp)://(\S*?\.\S*?))([\s)\[\]{},;"\':<]|\.\s|$)#i',
+                  "<a href=\"$1\" target=\"_blank\">$3</a>$4",
+                  $field
+                );
+                $words = explode(' ', $field);
+                $numberWords = 50;
+                for ($i = 0; $i < $numberWords; $i++) {
+                  echo $words[$i];
+                  if ($i == $numberWords) {
+                    echo '...';
+                  } else {
+                    echo ' ';
                   }
-                  }else{
-                 ?>
-                  <?php
-             echo "no result";
-         } ?>
+                }
+                ?>
+              </p>
+            </div>
+          <?php endwhile; ?>
+        <?php else: echo "no result"; ?>
+        <?php endif; ?>
       </div>
+
     </div>
   </div>
+
 </main>
 <?php get_footer();?>
