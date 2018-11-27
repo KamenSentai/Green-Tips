e2178076293056b84df95e67c1edd3d218331c2d
FlorianDurand <contact@floriandurand.com>
2018-11-27 23:03:56 +0100

    feat(404): error 404

---
+++ app/green-tips/404.php
@@ -0,0 +1,6 @@
+<?php get_header(); ?>
+<main id="content" >
+<h1 class="error"> Oops erreur 404, il semble que vous soyez égaré, profitez en pour regarder nos dernières enquêtes</h1>
+<?php include LAYOUTS_PATH . '/collection.php'; ?>
+</main>
+<?php get_footer(); ?>
